c5169925c053a545e49cfe99b27820803ce5ea3b
Daniel-Sebastian Buhaianu <dsb99.dev@gmail.com>
2023-04-06 15:02:31 +0300

    added the vectorized version of adjust_brightness func.

--- transform.py
+++ transform.py
@@ -2,19 +2,22 @@ from image import Image
 import numpy as np
 
 def adjust_brightness(image, factor):
-    # when we adjust the brightness, we just want
-    # to scale each value by some amount
-    # factor is a value > 0, how much you want to brigthen
-    # the image by (< 1 = darken, > 1 = brigthen)
+    # when we adjust the brightness, we just want to scale each value by some amount
+    # factor is a value > 0, how much you want to brigthen the image by 
+    # (< 1 = darken, > 1 = brigthen)
     x_pixels, y_pixels, num_channels = image.array.shape
 
-    # make an empty image so we don't mutate
-    # the one we've got
+    # make an empty image so we don't mutate the one we've got
     new_img = Image(x_pixels=x_pixels, y_pixels=y_pixels, num_channels=num_channels)
-    for x in range(x_pixels):
-        for y in range(y_pixels):
-            for c in range(num_channels):
-                new_img.array[x, y, c] = image.array[x, y, c] * factor
+
+    # non-vectorized implementation
+    # for x in range(x_pixels):
+    #     for y in range(y_pixels):
+    #         for c in range(num_channels):
+    #             new_img.array[x, y, c] = image.array[x, y, c] * factor
+
+    # vectorized version
+    new_img.array = image.array * factor
     
     return new_img
 
@@ -23,6 +26,10 @@ if __name__ == "__main__":
     lake = Image(filename="lake.png")
     city = Image(filename="city.png")
 
-    # let's lighten the lake
+    # brighten
     brightened_img = adjust_brightness(lake, 1.7)
     brightened_img.write_image("brightened.png")
+
+    # darken
+    darkened_img = adjust_brightness(lake, 0.3)
+    darkened_img.write_image("darkened.jpg")
