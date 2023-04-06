6021d737d7dca799198a4f5886718f038b2bf31a
Daniel-Sebastian Buhaianu <dsb99.dev@gmail.com>
2023-04-06 15:10:27 +0300

    implemented adjust_contrast func

--- transform.py
+++ transform.py
@@ -21,15 +21,39 @@ def adjust_brightness(image, factor):
     
     return new_img
 
+def adjust_contrast(image, factor, mid=0.5):
+    # adjust the contrast by increasing the difference
+    # from the user-defined midpoint by some amount, factor
+    x_pixels, y_pixels, num_channels = image.array.shape
+    new_img = Image(x_pixels=x_pixels, y_pixels=y_pixels, num_channels=num_channels)
+
+    # non-vectorized
+    # for x in range(x_pixels):
+    #     for y in range(y_pixels):
+    #         for c in range(num_channels):
+    #             new_img.array[x, y, c] = (image.array[x, y, c] - mid) * factor + mid
+
+    # vectorized
+    new_img.array = (image.array - mid) * factor + mid
+
+    return new_img
 
 if __name__ == "__main__":
     lake = Image(filename="lake.png")
     city = Image(filename="city.png")
 
-    # brighten
-    brightened_img = adjust_brightness(lake, 1.7)
-    brightened_img.write_image("brightened.png")
+    # # brighten
+    # brightened_img = adjust_brightness(lake, 1.7)
+    # brightened_img.write_image("brightened.png")
 
-    # darken
-    darkened_img = adjust_brightness(lake, 0.3)
-    darkened_img.write_image("darkened.jpg")
+    # # darken
+    # darkened_img = adjust_brightness(lake, 0.3)
+    # darkened_img.write_image("darkened.jpg")
+
+    # adjust the contract for the lake
+    incr_contrast = adjust_contrast(lake, 2, 0.5)
+    incr_contrast.write_image("incr_contrast.png")
+
+    # decrease contrast
+    decr_contrast = adjust_contrast(lake, 0.5, 0.5)
+    decr_contrast.write_image("decr_contrast.png")
