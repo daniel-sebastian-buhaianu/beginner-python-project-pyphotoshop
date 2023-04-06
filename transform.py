49ce72b9ecae87d9b94140531f476c10915cf7d7
Daniel-Sebastian Buhaianu <dsb99.dev@gmail.com>
2023-04-06 15:49:24 +0300

    implemented apply kernel

--- transform.py
+++ transform.py
@@ -58,6 +58,32 @@ def blur(image, kernel_size):
 
     return new_img
 
+def apply_kernel(image, kernel):
+    # the kernel should be a 2D array that represents the kernel we'll use
+    # for the sake of simplicity, we'll assume the kernel is SQUARE
+    # for example the sobel x kernel (detecting horizontal edges) is as follows:
+    # [1 0 -1]
+    # [2 0 -2]
+    # [1 0 -1]
+    x_pixels, y_pixels, num_channels = image.array.shape
+    new_img = Image(x_pixels=x_pixels, y_pixels=y_pixels, num_channels=num_channels)
+
+    kernel_size = kernel.shape[0]
+    neighbor_range = kernel_size // 2 # how many neighbors/side to check
+    for x in range(x_pixels):
+        for y in range(y_pixels):
+            for c in range(num_channels):
+                total = 0
+                for x_i in range(max(0, x-neighbor_range), min(x_pixels-1, x+neighbor_range) + 1):
+                    for y_i in range(max(0, y-neighbor_range), min(y_pixels-1, y+neighbor_range) + 1):
+                        # we need to find which value of the kernel this corresponds to
+                        x_k = x_i + neighbor_range - x
+                        y_k = y_i + neighbor_range - y
+                        kernel_val = kernel[x_k, y_k]
+                        total += image.array[x_i, y_i, c] * kernel_val
+                new_img.array[x, y, c] = total
+
+    return new_img
 
 if __name__ == "__main__":
     lake = Image(filename="lake.png")
@@ -79,10 +105,28 @@ if __name__ == "__main__":
     # decr_contrast = adjust_contrast(lake, 0.5, 0.5)
     # decr_contrast.write_image("decr_contrast.png")
 
-    # blur with kernel 3
-    blur_3 = blur(city, 3)
-    blur_3.write_image("blur_k3.png")
+    # # blur with kernel 3
+    # blur_3 = blur(city, 3)
+    # blur_3.write_image("blur_k3.png")
 
-    # blur with kernel 15
-    blur_15 = blur(city, 15)
-    blur_15.write_image("blur_k15.png")
+    # # blur with kernel 15
+    # blur_15 = blur(city, 15)
+    # blur_15.write_image("blur_k15.png")
+
+    # let's apply a sobel edge detection kernel on the x and y axis
+    sobel_x_kernel = np.array([
+        [1, 2, 1],
+        [0, 0, 0],
+        [-1, -2, -1]
+    ])
+
+    sobel_y_kernel = np.array([
+        [1, 0, -1],
+        [2, 0, -2],
+        [1, 0, -1]
+    ])
+
+    sobel_x = apply_kernel(city, sobel_x_kernel)
+    sobel_x.write_image("edge_x.png")
+    sobel_y = apply_kernel(city, sobel_y_kernel)
+    sobel_y.write_image("edge_y.png")
